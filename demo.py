59f3e548f9a1c547bdfd60125a3662b06e8356bf
Lz1427611970 <448254817@qq.com>
2020-11-11 22:52:30 +0800

    main

--- demo.py
+++ demo.py
@@ -1,2 +1,15 @@
-print("你好，无敌")
-print("hh")
+# print("你好，无敌")
+# print("hh")
+
+# b = type("哈哈哈")
+# print(b)
+
+# a = float(input("请输入一个数字："))
+# b = float(input("请输入另一个数字："))
+# print(a+b)
+# len()只能用在元祖数组字典中
+# print(len("hhhhhhhh"))
+
+
+
+
